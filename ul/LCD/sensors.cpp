11b7928f105184e4ae152b50d34c9eafab69a0a6
Ahmet Eren Caliskan <71254335+aeclskn@users.noreply.github.com>
2023-05-02 09:19:03 -0400

    Update sensors.cpp

--- ul/LCD/sensors.cpp
+++ ul/LCD/sensors.cpp
@@ -6,6 +6,14 @@ Sensors::Sensors(const QString& labelText, const QString& data, const QString& u
     QString fullLabelText = QString("%1: %2 %3").arg(labelText, data, unit);
     dataLabel_ = new QLabel(fullLabelText, this);
 
+    // Set the alignment of the label to top-left corner
+    dataLabel_->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+
+    // Set the font size to 14
+    QFont font = dataLabel_->font();
+    font.setPointSize(14);
+    dataLabel_->setFont(font);
+
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(dataLabel_);
     setLayout(layout);
@@ -16,4 +24,3 @@ void Sensors::setDataLabel(const QString& data)
     QString labelText = QString("%1").arg(data);
     dataLabel_->setText(labelText);
 }
-
